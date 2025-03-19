0577b167f7ed7af8db1e5b54fa66e8ceffd63ab7
Cai Silva <crs_caique@hotmail.com>
2025-03-20 10:43:41 +1300

    New commitement 20/03

--- back_end_notes/urls.py
+++ back_end_notes/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path, include
-
-def home(request):
-    return HttpResponse("Hello World")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
